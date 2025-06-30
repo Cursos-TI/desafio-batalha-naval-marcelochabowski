7791d662211efc0428a98b570e3f93938a86e0b6
marcelochabowski <104426941+marcelochabowski@users.noreply.github.com>
2025-06-30 08:29:40 -0300

    ajuste

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TAM 10 // usando um macro para define o tabulheiro
-const char agua = 'a'; 
+#define TAM 10 // usando um macro para definir o tabuleiro
+const char AGUA = 'a'; 
 const char NAVIO_JOGADOR = 'j';
 const char NAVIO_CPU = 'c';
 const char TIRO_AGUA = 'O';
@@ -20,15 +20,15 @@ void imprimirCelula(char c){
 }
 void imprimirTabuleiro(char tabuleiro[TAM][TAM]) {
     printf("  ");
-    for (int j = 0; j < TAMANHO; j++)
+    for (int j = 0; j < TAM; j++)
        printf("%2d ",j);
     printf("\n");
-    for(int i = 0; i < TAMANHO; i++){
+    for(int i = 0; i < TAM; i++){
         printf("%2d ",i);
-        for (int j = 0; j < TAMANHO; j++) {
+        for (int j = 0; j < TAM; j++) {
           imprimirCelula(tabuleiro[i][j]);
         }
-    printf("\n");    
+        printf("\n");    
     }
 }
 // usando funcao imprimircelular/tabuleiro e variavel para deixa o jogo visual
@@ -44,28 +44,46 @@ void posicionarNavio(char tabuleiro[TAM][TAM], char navio, int *x, int *y){
 // while para colocar aleatrorio
 int main(){
     char tabuleiro[TAM][TAM];
-    int xJog,yJog,xCPU,yCPU;
+    int xJog, yJog, xCPU, yCPU;
     int acertouJogador = 0, acertouCPU = 0;
     int tentativasJogador = 0, tentativasCPU = 0;
     srand(time(NULL));
-}
-// usando a funcao int main cria pontuacao e usar o tabuleiro unico para ambos jogadores e srand para gerar jogo aleatorios
- for (int i =0; i < TAMANHO; i++)
-     for (int j = 0, j < TAMANHO; j++)
-     tabuleiro[i][j] = AGUA;
-    posicionarNavio(tabuleiro, NAVIO_JOGADOR, &xJog,&yJog);
-    posicionarNavio(tabuleiro, NAVIO_CPU,&xCPU,&yJog);
- // variavel para posicionar navios
- printf("Bem-vindo a batalha naval\n');
-    while (!acertouJOGADOR && !acertouCPU)
 
-                               
+    // Inicializa o tabuleiro com água
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            tabuleiro[i][j] = AGUA;
 
+    // Posiciona os navios
+    posicionarNavio(tabuleiro, NAVIO_JOGADOR, &xJog, &yJog);
+    posicionarNavio(tabuleiro, NAVIO_CPU, &xCPU, &yCPU);
 
+    printf("Bem-vindo a batalha naval\n");
+    while (!acertouJogador && !acertouCPU) {
+        imprimirTabuleiro(tabuleiro);
+        int x, y;
+        printf("\nSua vez!\n");
+        printf("digite a linha (0-%d): ", TAM - 1);
+        scanf("%d", &x);
+        printf("digite a coluna (0-%d): ", TAM - 1);
+        scanf("%d", &y);
 
+        if (x < 0 || x >= TAM || y < 0 || y >= TAM) {
+            printf("Coordenadas invalidas. Tente novamente.\n");
+        } else if (tabuleiro[x][y] == NAVIO_CPU) {
+            printf("Acertou!\n");
+            tabuleiro[x][y] = TIRO_NAVIO;
+            acertouJogador++;
+        } else if (tabuleiro[x][y] == AGUA) {
+            printf("Tiro na agua.\n");
+            tabuleiro[x][y] = TIRO_AGUA;
+        } else {
+            printf("Ja atirou aqui.\n");
+        }
+        if (acertouJogador) break;
+        // Aqui você pode implementar a jogada da CPU se desejar
+    }
 
-
-
-
+    printf("Fim de jogo!\n");
     return 0;
 }
